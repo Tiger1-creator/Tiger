7701e3c4cd5f4080ea1235ec78679af7c4937c9c
Tiger1-creator <olivierniyongabo200@gmail.com>
2025-03-22 16:00:32 +0200

    for loop

---
+++ niyo.py
@@ -0,0 +1,20 @@
+def calc():
+    for i in range(10):
+        print (i)
+# calc()
+# def number():
+#     for i in range(20):
+#      if i % 2 == 0:
+#         print(i)
+#         i += 1
+# number()
+    
+# def num():
+#  n=1
+#  while n <= 20:
+#     if n % 2 == 0:
+#      print(n)
+#      if n==14:
+#       break
+#     n += 1
+# num()
